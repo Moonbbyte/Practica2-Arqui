f195979caeaa62e8593e16b5c7d05d79487cf628
Wilson Kevin Javier Chávez Cabrera <3016980070101@ingenieria.usac.edu.gt>
2022-02-22 17:31:03 -0600

    Respuesta 1 esclavo a maestro

--- JavsKrow/Arduino/Master/Master.ino
+++ JavsKrow/Arduino/Master/Master.ino
@@ -1,102 +1,60 @@
 #include <LiquidCrystal.h>
 #include <Wire.h>
-#include <Servo.h>
 
+// LCD
+int rs = 47, rw = 45, en = 43, d4 = 29, d5 = 27, d6 = 25, d7 = 23;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7); //Rs, en ,D3,D2,D1,D0
 
-//Sensor 
-int TRIG = 52;
-int ECO = 53;
-int DURACION;
-int DISTANCIA;
-
-
-//servo
-Servo servomec;
-//LCS
-int rs=47, rw=45, en = 43, d4=29, d5=27, d6=25, d7=23; 
-LiquidCrystal lcd(rs,en,d4,d5,d6,d7); //Rs, en ,D3,D2,D1,D0
-byte customChar[] = {
-  B00000,
-  B00000,
-  B01010,
-  B10001,
-  B01110,
-  B00000,
-  B00000,
-  B00000
-};
-
-
-void setup()
-{
-  pinMode(TRIG,OUTPUT);
-  pinMode(ECO, INPUT);
-  
-  Wire.begin();        // Conexión al Bus I2C
-  for (int i = 23; i<30;i++) pinMode(i,OUTPUT);
-  for (int i = 45; i<48;i++) pinMode(i,OUTPUT);
-  lcd.begin(16,2); //Columnas , Lineas
-  Serial.begin(9600);  // Velocidad de conexión
-  servomec.attach(32);//Pin
+void setup() {
+  Serial.begin(9600);
+  // LCD
+  for (int i = 23; i < 30; i++) pinMode(i, OUTPUT);
+  for (int i = 45; i < 48; i++) pinMode(i, OUTPUT);
+  lcd.begin(16, 2);
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  // I2C
+  Wire.begin();
+  Wire.beginTransmission(2);
+  Wire.write(0);
+  Wire.endTransmission();
+  Wire.requestFrom(2, 1);
+  char c = Wire.read();
+  if ((int)c == 1) {
+    mostrarMensaje();
+  }
 }
 
-void loop()
-{
-  String cadena = "";
-  lcd.setCursor(0,0); //Columna, Linea
-  lcd.print("Probando I2C");
-  Wire.requestFrom(2,3);    // Le pide 14 bytes al Esclavo 2
-//Servo
-//  servomec.write(0);
-//  delay(2000);
-//  servomec.write(90);
-//  delay(2000);
-//  servomec.write(180);
-//  delay(2000);
-  
+void loop() {
 
-//I2C
+}
 
-  while(Wire.available())    // slave may send less than requested
-  { 
-    char c = Wire.read();  
-    
-    Serial.print(c);
-    cadena += c;
-  }
-  lcd.setCursor(0,1);
-  lcd.print(cadena);
-  cadena = "";
-  Serial.println(cadena);
-
-  //Sensor
-//  digitalWrite(TRIG,HIGH);
-//  delay(1);
-//  digitalWrite(TRIG,LOW);
-//  DURACION = pulseIn(ECO,HIGH);
-//  DISTANCIA = DURACION /58.2;
-//  
-//  lcd.clear();
-//  lcd.setCursor(0,0); //Columna, Linea
-//  lcd.print(DISTANCIA );
-//  
-
-
-// Sensor 2
-//lcd.setCursor(0,0);
-//digitalWrite(TRIG,LOW);
-//delayMicroseconds(2);
-//digitalWrite(TRIG,HIGH);
-//delayMicroseconds(10);
-//digitalWrite(TRIG,LOW);
-//DURACION = pulseIn(ECO,HIGH);
-//
-//DISTANCIA = (DURACION *0.034)/2;
-//
-//Serial.println(DISTANCIA);
-//lcd.print("Distancia: ");
-//lcd.print(DISTANCIA);
-//lcd.print(" cm");
-
-delay(500);
+void mostrarMensaje() {
+  byte cara[] = {
+    B00000,
+    B00000,
+    B01010,
+    B10101,
+    B00000,
+    B10001,
+    B01110,
+    B00000
+  };
+  byte cheque[] = {
+    B00000,
+    B00000,
+    B00000,
+    B00001,
+    B10010,
+    B01100,
+    B01000,
+    B00000
+  };
+  lcd.createChar(0, cara);
+  lcd.setCursor(0, 0);
+  lcd.write(byte(0));
+  lcd.print("GRUPO 04 ACE");
+  lcd.createChar(1, cheque);
+  lcd.setCursor(13, 0);
+  lcd.write(byte(1));
 }
