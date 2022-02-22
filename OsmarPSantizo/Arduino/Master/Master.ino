6015ec51c86f11ec2b1d8cba1267585442d3522e
OsmarPSantizo <69328909+OsmarPSantizo@users.noreply.github.com>
2022-02-21 22:36:24 -0600

    Arduino

--- OsmarPSantizo/Arduino/Master/Master.ino
+++ OsmarPSantizo/Arduino/Master/Master.ino
@@ -3,100 +3,99 @@
 #include <Servo.h>
 
 
-//Sensor 
+//Sensor
 int TRIG = 52;
 int ECO = 53;
 int DURACION;
 int DISTANCIA;
-
+byte cara[] = {
+  B00000,
+  B00000,
+  B01010,
+  B10101,
+  B00000,
+  B10001,
+  B01110,
+  B00000
+};
+byte cheque[] = {
+  B00000,
+  B00000,
+  B00000,
+  B00001,
+  B10010,
+  B01100,
+  B01000,
+  B00000
+};
 
 //servo
 Servo servomec;
 //LCS
-int rs=47, rw=45, en = 43, d4=29, d5=27, d6=25, d7=23; 
-LiquidCrystal lcd(rs,en,d4,d5,d6,d7); //Rs, en ,D3,D2,D1,D0
-byte customChar[] = {
-  B00000,
-  B00000,
-  B01010,
-  B10001,
-  B01110,
-  B00000,
-  B00000,
-  B00000
-};
-
+int rs = 47, rw = 45, en = 43, d4 = 29, d5 = 27, d6 = 25, d7 = 23;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7); //Rs, en ,D3,D2,D1,D0
 
 void setup()
 {
-  pinMode(TRIG,OUTPUT);
+  pinMode(TRIG, OUTPUT);
   pinMode(ECO, INPUT);
   
   Wire.begin();        // Conexión al Bus I2C
-  for (int i = 23; i<30;i++) pinMode(i,OUTPUT);
-  for (int i = 45; i<48;i++) pinMode(i,OUTPUT);
-  lcd.begin(16,2); //Columnas , Lineas
+  for (int i = 23; i < 30; i++) pinMode(i, OUTPUT);
+  for (int i = 45; i < 48; i++) pinMode(i, OUTPUT);
+  lcd.begin(16, 2); //Columnas , Lineas
   Serial.begin(9600);  // Velocidad de conexión
   servomec.attach(32);//Pin
+  lcd.createChar(0, cara);
+  
 }
 
 void loop()
 {
-  String cadena = "";
-  lcd.setCursor(0,0); //Columna, Linea
-  lcd.print("Probando I2C");
-  Wire.requestFrom(2,3 );    // Le pide 14 bytes al Esclavo 2
-//Servo
-  servomec.write(0);
-  delay(2000);
-//  servomec.write(90);
-//  delay(2000);
-//  servomec.write(180);
-//  delay(2000);
   
-
-//I2C
-
-  while(Wire.available())    // slave may send less than requested
-  { 
-    char c = Wire.read();  
-    
-    Serial.print(c);
-    cadena += c;
-  }
-  lcd.setCursor(0,1);
+  String cadena = "";lcd.setCursor(0, 1); //Columna, Linea
+  lcd.write(byte(0));
+  lcd.setCursor(1, 1);
   lcd.print(cadena);
   cadena = "";
   Serial.println(cadena);
-
-  //Sensor
-//  digitalWrite(TRIG,HIGH);
-//  delay(1);
-//  digitalWrite(TRIG,LOW);
-//  DURACION = pulseIn(ECO,HIGH);
-//  DISTANCIA = DURACION /58.2;
-//  
-//  lcd.clear();
-//  lcd.setCursor(0,0); //Columna, Linea
-//  lcd.print(DISTANCIA );
-//  
+  lcd.print("GRUPO 04 ACE");
+  lcd.createChar(1, cheque);
+  lcd.setCursor(13, 1); //Columna, Linea
+  lcd.write(byte(1));
+  Wire.beginTransmission(2);
+  Wire.write(1);
+  Wire.endTransmission();
+  
 
 
-// Sensor 2
-//lcd.setCursor(0,0);
-//digitalWrite(TRIG,LOW);
-//delayMicroseconds(2);
-//digitalWrite(TRIG,HIGH);
-//delayMicroseconds(10);
-//digitalWrite(TRIG,LOW);
-//DURACION = pulseIn(ECO,HIGH);
+  //I2C
+  int cadena2;
+
+  
+  while (true){
+    Wire.requestFrom(2,1);
+    char c = Wire.read();
+    
+    Serial.println((int)c-32);
+    lcd.setCursor(0, 1);
+    lcd.print((int)c);
+    delay(1000);
+  }
+  
+//  while (Wire.available()>0)   // slave may send less than requested
+//  {
+//    char c = Wire.read();
+//    cadena2 = String(c).toInt();
+//    Serial.println(cadena2);
+//    
+//    cadena += c;
+//  }
+//   //Columna, Linea
 //
-//DISTANCIA = (DURACION *0.034)/2;
-//
-//Serial.println(DISTANCIA);
-//lcd.print("Distancia: ");
-//lcd.print(DISTANCIA);
-//lcd.print(" cm");
+//  lcd.setCursor(1, 1);
+//  lcd.print(cadena);
+//  
 
-delay(500);
+  delay(500);
 }
--- OsmarPSantizo/Arduino/Slave/Slave.ino
+++ OsmarPSantizo/Arduino/Slave/Slave.ino
@@ -1,44 +1,110 @@
 #include <Wire.h>
+#include <Servo.h>
 int TRIG = 12;
 int ECO = 11;
 int DURACION;
 int DISTANCIA;
-
+int ESTADO = 0;
+char DISTANCIAC;
+//servo
+Servo servomec;
 void setup()
 {
-   Wire.begin(2);                // Este Esclavo es el número 2
-  Wire.onRequest(requestEvent); // Cuando el Maestro le hace una petición,
-     pinMode(TRIG,OUTPUT);
+  Wire.begin(2);                // Este Esclavo es el número 2
+
+  Wire.onReceive(requestEvent);
+  Wire.onRequest(datos); // Cuando el Maestro le hace una petición,
+  pinMode(TRIG, OUTPUT);
   pinMode(ECO, INPUT);                            // realiza el requestEvent
   Serial.begin(9600);
+  servomec.attach(8);//Pin
 }
 
 void loop()
 {
+  if(ESTADO ==0){
+    mensaje();
+  }
+
+  else if (ESTADO == 1) {
+    barrido();
+  } else if ( ESTADO == 2 ) {
+    Disparar();
+  }
   delay(500);
+
+}
+
+void datos() {
+  if(ESTADO == 0){
+    Wire.write(5);
+  }else if(ESTADO == 1){
+    Wire.write(DISTANCIAC + 32);  
+  }
+  
 }
 
 // Esto es lo que envía cuando le hace la petición.
 void requestEvent()
 {
 
-//digitalWrite(TRIG,LOW);
-//delayMicroseconds(2);
-//digitalWrite(TRIG,HIGH);
-//delayMicroseconds(10);
-//digitalWrite(TRIG,LOW);
-//DURACION = pulseIn(ECO,HIGH);
-//
-//DISTANCIA = (DURACION *0.034)/2;
-//Serial.println(byte(DISTANCIA));
+
+  int x = Wire.read();    // receive byte as an integer
+  if(x==0){
+    ESTADO = 0;
+  }
+  else if (x == 1 ) {
+    ESTADO = 1 ;
+  } else if (x == 2) {
+    ESTADO = 2;
+  }
+
+}
+
+void mensaje(){
+  Serial.println("Hola");
+}
+
+void barrido() {
+  for (int i = 0 ; i < 180 ; i += 10) {
+    servomec.write(i);
+    digitalWrite(TRIG, LOW);
+    delayMicroseconds(2);
+    digitalWrite(TRIG, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIG, LOW);
+    DURACION = pulseIn(ECO, HIGH);
+
+    DISTANCIA = (DURACION * 0.034) / 2;
+
+    DISTANCIAC = (char)DISTANCIA;
+
+    Serial.println(DISTANCIAC);
+
+    //    Wire.write(53);
+    delay(1000);
+  }
 
 
+  for (int i = 180 ; i >= 0 ; i -= 10) {
+    servomec.write(i);
+    digitalWrite(TRIG, LOW);
+    delayMicroseconds(2);
+    digitalWrite(TRIG, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIG, LOW);
+    DURACION = pulseIn(ECO, HIGH);
+
+    DISTANCIA = (DURACION * 0.034) / 2;
+    DISTANCIAC = (char)DISTANCIA;
+    Serial.println(DISTANCIAC );
 
 
-char letras[3] = {B00110010,B00110000,B00100000};
+    delay(1000);
+  }
+}
 
-Wire.write(letras);
+void Disparar() {
+  Serial.println("HOLA");
 
-  
-        
 }
