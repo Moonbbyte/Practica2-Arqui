013d51e4d965e604c185b1a48f0346d019f5b293
Gerson Quintana <gersonquintana444@gmail.com>
2022-02-22 23:16:16 -0600

    2

--- GersonQuintana/Arduino/Master/Master.ino
+++ GersonQuintana/Arduino/Master/Master.ino
@@ -1,146 +1,181 @@
 #include <LiquidCrystal.h>
 #include <Wire.h>
-#include <Servo.h>
 
-
-//Sensor
-int TRIG = 52;
-int ECO = 53;
-int DURACION;
-int DISTANCIA;
-
-byte cara[] = {
-  B00000,
-  B00000,
-  B01010,
-  B10101,
-  B00000,
-  B10001,
-  B01110,
-  B00000
-};
-byte cheque[] = {
-  B00000,
-  B00000,
-  B00000,
-  B00001,
-  B10010,
-  B01100,
-  B01000,
-  B00000
-};
-
-// Distancias de los enemigos ---------------------------------------------------------
-int distancias1[18] = {3, 1, 9, 3, 4, 5, 6, 7, 8, 9, 88, 11, 12, 13, 101, 15, 16, 200};
-int distancias2[18] = {3, 1, 9, 3, 4, 5, 6, 7, 8, 9, 88, 11, 12, 13, 101, 15, 16, 200};
-bool iguales = true; // Si los arreglos de las distancias son iguales
-int nPos = 0; // Contador del numero de posiciones enviadas
-
-
-//servo
-Servo servomec;
-//LCS
+// LCD
 int rs = 47, rw = 45, en = 43, d4 = 29, d5 = 27, d6 = 25, d7 = 23;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7); //Rs, en ,D3,D2,D1,D0
+// Mensaje
+long unsigned tiempo_msj;
+// Variables
+int contador_loop = 0;
+int btn_barrido = 24;
+int btn_ataque = 26;
+int btn_pausa = 28;
+bool btn_barrido_pres, btn_ataque_pres, btn_pausa_pres;
 
-void setup()
-{
-  pinMode(TRIG, OUTPUT);
-  pinMode(ECO, INPUT);
 
-  Wire.begin();        // Conexión al Bus I2C
+int distancias1[18] = {3, 1, 9, 3, 4, 5, 6, 7, 8, 9, 88, 11, 12, 13, 101, 15, 16, 200};
+int distancias2[18] = {3, 1, 9, 3, 4, 5, 6, 7, 8, 9, 88, 11, 12, 13, 101, 15, 16, 200};
+bool iguales = true;
+int nPos = 0;
+
+void setup() {
+  Serial.begin(9600);
+  // LCD
   for (int i = 23; i < 30; i++) pinMode(i, OUTPUT);
   for (int i = 45; i < 48; i++) pinMode(i, OUTPUT);
-  lcd.begin(16, 2); //Columnas , Lineas
-  Serial.begin(9600);  // Velocidad de conexión
-  servomec.attach(32);//Pin
-  lcd.createChar(0, cara);
-
+  lcd.begin(16, 2);
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  // I2C - Mensaje Inicial (respuesta esclavo)
+  Wire.begin();
+  Wire.beginTransmission(2);
+  Wire.write(0);
+  Wire.endTransmission();
+  Wire.requestFrom(2, 1);
+  char c = Wire.read();
+  if ((int)c == 1) {
+    mensajeInicial();
+    tiempo_msj = millis();
+  }
 }
 
-void loop()
-{
-  String cadena = ""; lcd.setCursor(0, 1); //Columna, Linea
+void loop() {
+  mensajeReinicioLoop();
+  resetearParametros();
+  esperarInstrucciones();
+}
+
+void mensajeInicial() {
+  byte cara[] = {
+    B00000,
+    B00000,
+    B01010,
+    B10101,
+    B00000,
+    B10001,
+    B01110,
+    B00000
+  };
+  byte cheque[] = {
+    B00000,
+    B00000,
+    B00000,
+    B00001,
+    B10010,
+    B01100,
+    B01000,
+    B00000
+  };
+  lcd.createChar(0, cara);
+  lcd.setCursor(0, 0);
   lcd.write(byte(0));
-  lcd.setCursor(1, 1);
-  lcd.print(cadena);
-  cadena = "";
-  Serial.println(cadena);
+  lcd.setCursor(1, 0);
   lcd.print("GRUPO 04 ACE");
   lcd.createChar(1, cheque);
-  lcd.setCursor(13, 1); //Columna, Linea
+  lcd.setCursor(13, 0);
   lcd.write(byte(1));
-  Wire.beginTransmission(2);
-  Wire.write(1);
-  Wire.endTransmission();
+}
 
-
-
-  //I2C
-  int cadena2;
-
-
+void mensajeReinicioLoop() {
   while (true) {
+    if ((millis() - tiempo_msj) > 10000) {
+      contador_loop++;
+      lcd.setCursor(0, 0);
+      lcd.print("Entrando al Loop");
+      lcd.setCursor(0, 1);
+      lcd.print("Contador: ");
+      lcd.print(contador_loop);
+      delay(1000);
+      break;
+    }
+  }
+}
 
-    //    Wire.requestFrom(2,1);
-    //    char c = Wire.read();
+void resetearParametros() {
+  // LCD
+  lcd.clear();
+  lcd.setCursor(0, 0);
+}
 
-    //      int pos = (int)c-32; // posicion del enemigo
+void esperarInstrucciones() {
+  recibirDistancias();
+  while (true) {
+    // Instruccion barrido
+    if (!estaPresionado(btn_barrido)) {
+      btn_barrido_pres = true;
+    }
 
-    //    Serial.println((int)c-32);
-    //    lcd.setCursor(0, 1);
-    //    lcd.print((int)c);
-    //    delay(1000);
+    if (estaPresionado(btn_barrido) && btn_barrido_pres) {
+      // Accion
+      btn_barrido_pres = false;
+      
+      recibirDistancias();
+    }
+    // Instruccion ataque
+    if (!estaPresionado(btn_ataque)) {
+      btn_ataque_pres = true;
+    }
+    if (estaPresionado(btn_ataque) && btn_ataque_pres) {
+      // Accion
+      btn_ataque_pres = false;
+    }
+    // Instruccion pausa
+    if (!estaPresionado(btn_pausa)) {
+      btn_pausa_pres = true;
+    }
+    if (estaPresionado(btn_pausa) && btn_pausa_pres) {
+      // Accion
+      btn_pausa_pres = false;
+    }
+    // Si no tiene instrucciones
+    if (!estaPresionado(btn_barrido) && !estaPresionado(btn_ataque) && !estaPresionado(btn_pausa)) {
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Esperando");
+      lcd.setCursor(0, 1);
+      lcd.print("instruccion...");
+    }
+    delay(1000);
+  }
+}
 
+bool estaPresionado(int idBtn) {
+  int estadoBtn = digitalRead(idBtn);
+  return (estadoBtn == HIGH);
+}
 
-    // ------------------------------------- INICIO AGREGADO -----------------------------------------------
-    // ------------------------------------- Descomentar despues -----------------------------------------------
+void recibirDistancias() {
+  
+  // Se deberia de enviar algo al maestro para que empiece a escanear
+  while (true) {
+    Wire.requestFrom(2, 1);
+    char c = Wire.read();
+    int pos = (int)c - 32;
 
     // Hay que recibir constantemente los datos
-    //    if (nPos < 18) { // Llenando la lista 1
-    //      distancias1[nPos] = pos;
-    //    }
-    //    else if (nPos < 36) { // Llenando la lista 2
-    //      distancias2[nPos - 18] = pos;
-    //    }
-    //    else {
-    
-            // Cuando ya se hayan enviado las dos listas, entrará aquí
-            bool c = compararPosiciones(); // Comparando cada una de las posiciones de los dos arreglos para ver si son iguales
-            if (c) {  // Son iguales las distancias
-              ordenarLista();
-              enviarDistancias();
-            } else { // Las distancias son diferentes
-              Wire.beginTransmission(2); // inciando la transmision de datos
-              Wire.write(3);
-              Wire.write('n'); // NO son iguales las distancias
-              Wire.endTransmission();
-            }
-            break;
-    //}
-
-    // ------------------------------------- Fin Descomentar -----------------------------------------------
-    // ------------------------------------- FIN AGREGADO -----------------------------------------------
-
-    delay(500);
-
-  }
-
-  //  while (Wire.available()>0)   // slave may send less than requested
-  //  {
-  //    char c = Wire.read();
-  //    cadena2 = String(c).toInt();
-  //    Serial.println(cadena2);
-  //
-  //    cadena += c;
-  //  }
-  //   //Columna, Linea
-  //
-  //  lcd.setCursor(1, 1);
-  //  lcd.print(cadena);
-  //
-
-  delay(500);
+    if (nPos < 18) { // Llenando la lista 1
+      distancias1[nPos] = pos;
+    }
+    else if (nPos < 36) { // Llenando la lista 2
+      distancias2[nPos - 18] = pos;
+    }
+    else {
+        // Cuando ya se hayan enviado las dos listas, entrará aquí
+        bool c = compararPosiciones();
+        if (c) {  
+          ordenarLista();
+          enviarDistancias();
+        } else { 
+          Wire.beginTransmission(2); 
+          Wire.write(3);
+          Wire.write('n');
+          Wire.endTransmission();
+        }
+        break;
+    }
+    nPos++;
+  } // fin while
 }
 
 
@@ -181,5 +216,3 @@ void enviarDistancias() {
   Wire.write(100); // terminando la transmision de datos
   Wire.endTransmission();
 }
-
-// ------------------------------------- FIN AGREGADO -----------------------------------------------
--- GersonQuintana/Arduino/Slave/Slave.ino
+++ GersonQuintana/Arduino/Slave/Slave.ino
@@ -1,72 +1,52 @@
 #include <Wire.h>
 #include <Servo.h>
+
 int TRIG = 12;
 int ECO = 11;
-int DURACION;
-int DISTANCIA;
 int ESTADO = 0;
-char DISTANCIAC;
 
-int distancias[18]; // Se guardan las distancias enviadas desde el Maestro
+int distancias[18];
 
-//servo
-Servo servomec;
-
-void setup()
-{
-  Wire.begin(2);                // Este Esclavo es el número 2
-
-  Wire.onReceive(requestEvent); // Se ejecuta el metodo que esta entre parentesis
-  Wire.onRequest(datos); // Cuando el Maestro le hace una petición,
-  pinMode(TRIG, OUTPUT);
-  pinMode(ECO, INPUT);                            // realiza el requestEvent
+void setup() {
   Serial.begin(9600);
-  servomec.attach(8);//Pin
+  Serial.println("ESCLAVO");
+  // Sensor
+  pinMode(TRIG, OUTPUT);
+  pinMode(ECO, INPUT);
+  // I2C
+  Wire.begin(2);
+  Wire.onReceive(requestEvent);
+  Wire.onRequest(datos);
 }
 
-void loop()
-{
-  if (ESTADO == 0) {
-    mensaje();
-  }
-  else if (ESTADO == 1) {
-    barrido();
-  }
-  else if ( ESTADO == 2 ) {
-    Disparar();
-  }
-  delay(500);
+void loop() {
 
 }
 
 void datos() {
   if (ESTADO == 0) {
-    Wire.write(5);
+    Wire.write(1);
+    Serial.println("Si pide mensaje");
   }
   else if (ESTADO == 1) {
-    Wire.write(DISTANCIAC + 32);
+    barrido();
   }
-
 }
 
-// Esto es lo que envía cuando le hace la petición.
-void requestEvent()
-{
+void requestEvent() {
   int x = Wire.read();    // receive byte as an integer
   if (x == 0) {
     ESTADO = 0;
   }
-  else if (x == 1 ) {
-    ESTADO = 1 ;
+  else if (x == 1) {
+    ESTADO = 1;
   }
   else if (x == 2) {
     ESTADO = 2;
   }
-  
-  // ------------------------------------- INICIO AGREGADO -----------------------------------------------
-  else if (x == 3) { // Recibir distancias del arreglo
+  else if (x == 3) {
     char ok = Wire.read();
-    
+
     if (ok == 'o') { // Los arreglos son iguales
       int pos = 0;
       x = Wire.read();
@@ -81,58 +61,48 @@ void requestEvent()
         Serial.print(", ");
       }
     }
-    
     else {
       Serial.println("Hay distancias diferentes");
     }
   }
-  // ------------------------------------- FIN AGREGADO -----------------------------------------------
-
-}
-
-void mensaje() {
-  Serial.println("Hola");
+  
 }
 
 void barrido() {
-  for (int i = 0 ; i < 180 ; i += 10) {
-    servomec.write(i);
-    digitalWrite(TRIG, LOW);
-    delayMicroseconds(2);
-    digitalWrite(TRIG, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(TRIG, LOW);
-    DURACION = pulseIn(ECO, HIGH);
-
-    DISTANCIA = (DURACION * 0.034) / 2;
-
-    DISTANCIAC = (char)DISTANCIA;
-
-    Serial.println(DISTANCIAC);
-
-    //    Wire.write(53);
-    delay(1000);
-  }
-
-
-  for (int i = 180 ; i >= 0 ; i -= 10) {
-    servomec.write(i);
-    digitalWrite(TRIG, LOW);
-    delayMicroseconds(2);
-    digitalWrite(TRIG, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(TRIG, LOW);
-    DURACION = pulseIn(ECO, HIGH);
-
-    DISTANCIA = (DURACION * 0.034) / 2;
-    DISTANCIAC = (char)DISTANCIA;
-    Serial.println(DISTANCIAC );
-
-
-    delay(1000);
-  }
-}
-
-void Disparar() {
-  Serial.println("HOLA");
+  Serial.println("Se esta haciendo el barrido");  
+//  for (int i = 0 ; i < 180 ; i += 10) {
+//    servomec.write(i);
+//    digitalWrite(TRIG, LOW);
+//    delayMicroseconds(2);
+//    digitalWrite(TRIG, HIGH);
+//    delayMicroseconds(10);
+//    digitalWrite(TRIG, LOW);
+//    DURACION = pulseIn(ECO, HIGH);
+//
+//    DISTANCIA = (DURACION * 0.034) / 2;
+//
+//    DISTANCIAC = (char)DISTANCIA;
+//
+//    Serial.println(DISTANCIAC);
+//
+//    //    Wire.write(53);
+//    delay(1000);
+//  }
+//
+//
+//  for (int i = 180 ; i >= 0 ; i -= 10) {
+//    servomec.write(i);
+//    digitalWrite(TRIG, LOW);
+//    delayMicroseconds(2);
+//    digitalWrite(TRIG, HIGH);
+//    delayMicroseconds(10);
+//    digitalWrite(TRIG, LOW);
+//    DURACION = pulseIn(ECO, HIGH);
+//
+//    DISTANCIA = (DURACION * 0.034) / 2;
+//    DISTANCIAC = (char)DISTANCIA;
+//    Serial.println(DISTANCIAC );
+//
+//    delay(1000);
+//  }
 }
