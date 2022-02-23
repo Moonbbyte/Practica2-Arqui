a46690b4ae1ad567b0f4ce554d8a6f224b14c5a3
Wilson Kevin Javier Chávez Cabrera <3016980070101@ingenieria.usac.edu.gt>
2022-02-22 21:35:38 -0600

    Esperando instrucciones

--- JavsKrow/Arduino/Master/Master.ino
+++ JavsKrow/Arduino/Master/Master.ino
@@ -6,6 +6,12 @@ int rs = 47, rw = 45, en = 43, d4 = 29, d5 = 27, d6 = 25, d7 = 23;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7); //Rs, en ,D3,D2,D1,D0
 // Mensaje
 long unsigned tiempo_msj;
+// Variables
+int contador_loop = 0;
+int btn_barrido = 24;
+int btn_ataque = 26;
+int btn_pausa = 28;
+bool btn_barrido_pres, btn_ataque_pres, btn_pausa_pres;
 
 void setup() {
   Serial.begin(9600);
@@ -15,7 +21,7 @@ void setup() {
   lcd.begin(16, 2);
   lcd.clear();
   lcd.setCursor(0, 0);
-  // I2C
+  // I2C - Mensaje Inicial (respuesta esclavo)
   Wire.begin();
   Wire.beginTransmission(2);
   Wire.write(0);
@@ -23,23 +29,18 @@ void setup() {
   Wire.requestFrom(2, 1);
   char c = Wire.read();
   if ((int)c == 1) {
-    mostrarMensaje();
+    mensajeInicial();
     tiempo_msj = millis();
   }
 }
 
 void loop() {
-  if ((millis() - tiempo_msj) > 1000) {
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("Esperando");
-    lcd.setCursor(0, 1);
-    lcd.print("instruccion...");
-    delay(1000);
-  }
+  mensajeReinicioLoop();
+  resetearParametros();
+  esperarInstrucciones();
 }
 
-void mostrarMensaje() {
+void mensajeInicial() {
   byte cara[] = {
     B00000,
     B00000,
@@ -68,4 +69,65 @@ void mostrarMensaje() {
   lcd.createChar(1, cheque);
   lcd.setCursor(13, 0);
   lcd.write(byte(1));
+}
+
+void mensajeReinicioLoop() {
+  contador_loop++;
+  lcd.setCursor(0, 0);
+  lcd.print("Entrando al Loop");
+  lcd.setCursor(0, 1);
+  lcd.print("Contador: ");
+  lcd.print(contador_loop);
+  delay(1000);
+}
+
+void resetearParametros() {
+  // LCD
+  lcd.clear();
+  lcd.setCursor(0, 0);
+}
+
+void esperarInstrucciones() {
+  while (true) {
+    if ((millis() - tiempo_msj) > 10000) {
+      // Instruccion barrido
+      if (!estaPresionado(btn_barrido)) {
+        btn_barrido_pres = true;
+      }
+      if (estaPresionado(btn_barrido) && btn_barrido_pres) {
+        // Accion
+        btn_barrido_pres = false;
+      }
+      // Instruccion ataque
+      if (!estaPresionado(btn_ataque)) {
+        btn_ataque_pres = true;
+      }
+      if (estaPresionado(btn_ataque) && btn_ataque_pres) {
+        // Accion
+        btn_ataque_pres = false;
+      }
+      // Instruccion pausa
+      if (!estaPresionado(btn_pausa)) {
+        btn_pausa_pres = true;
+      }
+      if (estaPresionado(btn_pausa) && btn_pausa_pres) {
+        // Accion
+        btn_pausa_pres = false;
+      }
+      // Si no tiene instrucciones
+      if (!estaPresionado(btn_barrido) && !estaPresionado(btn_ataque) && !estaPresionado(btn_pausa)) {
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("Esperando");
+        lcd.setCursor(0, 1);
+        lcd.print("instruccion...");
+      }
+      delay(1000);
+    }
+  }
+}
+
+bool estaPresionado(int idBtn) {
+  int estadoBtn = digitalRead(idBtn);
+  return (estadoBtn == HIGH);
 }
